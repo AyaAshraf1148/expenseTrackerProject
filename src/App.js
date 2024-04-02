aae86d61c99fc6beac2d2dfc108e3962cd07fe08
Aya <ayaashraf1148@gmail.com>
2024-04-02 21:53:42 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransctionList from './components/TransctionList';
+import AddTransction from './components/AddTransction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <div className='container'>
+        <Balance/>
+        <IncomeExpenses/>
+        <TransctionList/>
+        <AddTransction/>
+      </div>
     </div>
   );
 }
